aa6f3d755743b3e666f628bb0e0e133a200bec94
Sarah Cohen <cohen.sarah591@gmail.com>
2023-10-09 19:35:39 -0400

    completed q5

--- lesson_1/lesson_work/HW_CH_4.sql
+++ lesson_1/lesson_work/HW_CH_4.sql
@@ -63,8 +63,18 @@ LEFT OUTER JOIN InvoiceLineItems i ON g.AccountNo = i.AccountNo
 WHERE i.AccountNo IS NULL
 ORDER BY g.AccountNo
 
---5. Use the UNION operator to generate a result set consisting of two columns from the Vendors table: VendorName and VendorState. 
---If the vendor is in California, the VendorState should be “CA”; otherwise, the VendorState value should be “Outside CA”. Sort the result set by VendorName
+--5. Use the UNION operator to generate a result set consisting of two columns from the Vendors table: 
+--VendorName and VendorState. 
+--If the vendor is in California, the VendorState should be “CA”; otherwise, the VendorState value should be “Outside CA”. 
+--Sort the result set by VendorName
+SELECT VendorName, 'CA' AS VendorState 
+FROM Vendors
+WHERE VendorState = 'CA'
+UNION
+SELECT VendorName, 'Outside CA' AS VendorState
+FROM Vendors
+WHERE VendorState != 'CA'
+ORDER BY VendorName
 
 /*
 Finance Database
