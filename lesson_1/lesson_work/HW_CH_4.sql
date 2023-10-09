f7aaacd92c4053b0a0d7444ed5a6eb472007677f
Sarah Cohen <cohen.sarah591@gmail.com>
2023-10-09 19:21:30 -0400

    fixed q3, completed q4

--- lesson_1/lesson_work/HW_CH_4.sql
+++ lesson_1/lesson_work/HW_CH_4.sql
@@ -44,9 +44,6 @@ ORDER BY g.AccountDescription, v.VendorName
 --ii. inv Invoice table
 --iii. ili InvoiceLineItems table
 --Sort the result set by Vendor, Date, Number, and #
-select * from Vendors order by VendorName
-select * from Invoices
-select * from InvoiceLineItems
 SELECT ven.VendorName AS [Vendor],
 	inv.InvoiceDate AS [Date],
 	inv.InvoiceNumber AS [Number],
@@ -57,8 +54,14 @@ JOIN Invoices inv ON ven.VendorID = inv.VendorID
 JOIN InvoiceLineItems ili ON inv.InvoiceID = ili.InvoiceID
 ORDER BY [Vendor], [Date], [Number], [#]
 
---4. Write a SELECT statement that returns two columns from the GLAccounts table: AccountNo and
---AccountDescription. The result set should have one row for each account number that has never been used. Sort the result set by AccountNo. [Hint: use an outer join to the InvoiceLineItems table]
+--4. Write a SELECT statement that returns two columns from the GLAccounts table: AccountNo and AccountDescription. 
+--The result set should have one row for each account number that has never been used. 
+--Sort the result set by AccountNo. [Hint: use an outer join to the InvoiceLineItems table]
+SELECT g.AccountNo, g.AccountDescription
+FROM GLAccounts g
+LEFT OUTER JOIN InvoiceLineItems i ON g.AccountNo = i.AccountNo 
+WHERE i.AccountNo IS NULL
+
 --5. Use the UNION operator to generate a result set consisting of two columns from the Vendors table: VendorName and VendorState. 
 --If the vendor is in California, the VendorState should be “CA”; otherwise, the VendorState value should be “Outside CA”. Sort the result set by VendorName
 
