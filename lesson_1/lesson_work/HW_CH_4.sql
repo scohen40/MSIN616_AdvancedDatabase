ee5243f45066c891ae511db88985f5c97ca2c33c
Sarah Cohen <cohen.sarah591@gmail.com>
2023-10-09 16:35:41 -0400

    added homework chapter 4 questions and completed Q1

---
+++ lesson_1/lesson_work/HW_CH_4.sql
@@ -0,0 +1,58 @@
+--Chapter 4 Homework
+--Sarah Cohen
+
+/*
+AP Database
+*/
+USE AP
+GO
+
+--1. Write a SELECT statement that returns four columns:
+-- a. VendorName from the Vendors table,
+-- b. InvoiceNumber from the Invoices table,
+-- c. InvoiceDate from the Invoices table,
+-- d. BalanceInvoiceTotal minus the sum of PaymentTotal and CreditTotal
+-- The result should have one row for each invoice with a non-zero balance. Sort the result by VendorName in ascending order.
+select * from Vendors
+select * from Invoices
+SELECT VendorName, 
+	InvoiceNumber, 
+	InvoiceDate,
+	InvoiceTotal - (PaymentTotal + CreditTotal) AS Balance
+FROM Invoices
+JOIN Vendors ON Invoices.VendorID = Vendors.VendorID
+WHERE InvoiceTotal - (PaymentTotal + CreditTotal) != 0
+ORDER BY VendorName ASC
+
+--2. Write a SELECT statement that returns three columns:
+-- a. VendorName from the Vendors table,
+-- b. DefaultAccountNo from the Vendors table,
+-- c. AccountDescription from GLAccounts table
+--The result should have one row for each vendor, with the account number and account description for the vendor’s default account number. Sort the result set by AccountDescription, then by VendorName
+--3. Write a SELECT statement that returns five columns from three tables, all using column aliases:
+-- a. Vendor VendorName column
+-- b. Date InvoiceDate column
+-- c. Number InvoiceNumber column
+-- d. # InvoiceSequence column
+-- e. LineItem InvoiceLineItemAmount column
+--Assign the following correlation names to the tables:
+--i. ven Vendors table
+--ii. inv Invoice table
+--iii. ili InvoiceLineItems table
+--Sort the result set by Vendor, Date, Number, and #
+--4. Write a SELECT statement that returns two columns from the GLAccounts table: AccountNo and
+--AccountDescription. The result set should have one row for each account number that has never been used. Sort the result set by AccountNo. [Hint: use an outer join to the InvoiceLineItems table]
+--5. Use the UNION operator to generate a result set consisting of two columns from the Vendors table: VendorName and VendorState. 
+--If the vendor is in California, the VendorState should be “CA”; otherwise, the VendorState value should be “Outside CA”. Sort the result set by VendorName
+
+/*
+Finance Database
+*/
+USE Finance
+GO
+
+--6. Write a SELECT statement that returns three columns: Date, closing price for IBM Ticker, and closing price for GLD ticker. The first few rows are shown below
+--7. Repeat the previous exercise, this time return the data only for the days on which GLD closed higher than IBM Library Database
+--8. Write a SELECT statement that returns the titles, ISBN numbers, and locations of all the books.
+--9. Write a SELECT statement that returns the titles of all books, their categories, genres, fields, subfields, and types. Arrange the columns in the order that makes most sense to you. A possible return set might begin:
+--10. Modify the query above to display ‘N/A’ or ‘---’ instead of NULL
