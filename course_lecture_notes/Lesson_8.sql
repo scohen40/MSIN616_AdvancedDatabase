17c2ee9abd9266921b03b9a0253e93d4704ab672
Sarah Cohen <cohen.sarah591@gmail.com>
2023-11-28 13:29:47 -0500

    added lesson 8 notes

--- course_lecture_notes/Lesson_8.sql
+++ course_lecture_notes/Lesson_8.sql
@@ -1,4 +1,4 @@
---Lesson 7
+--Lesson 8
 --11/27/2023
 
 /* Chapter 11 DDL */
@@ -53,3 +53,52 @@ GO -- to keep processing it, running multiple statements in quick succession
 --put foreign key constraints at the en, AFTER INSERTING ALL THE DATA. why? because we haven't yet inserted all of the data into the tables.
 --if i'm using invoiceLineItems as InvoiceID but I haven't added any Invoices, you can't use it.
 
+/* Chapter 12 */
+-- when we submit the create scripts it should be a .sql that she can run
+--look at slides for instructions on how to use the GUI to create a DB
+
+--Between chapters 11 and 12, should be able to create a db, add PKs, FKs, and constraints.
+
+
+/* Chapter 13 */
+--View
+-- a select statement that is stored as an object in the db
+-- used for commonly requested reports or dataset
+CREATE VIEW VendorsMin AS 
+	SELECT VendorName, VendorState, VendorPhone
+	FROM Vendors;
+-- and then
+SELECT *
+FROM VendorsMin
+--different from CTE/temp table which you have to run again and again and expose your code to whomever is running it
+--let's say you don't want people to know what the data is, just use it.
+--with a view the person can use the data without really knowing what the data actually is by creating a more permanent temp table.
+--why use a view?
+--1. Design Independence - if you add suddenly 90 col to Vendors table, it will be annoying to find the phone number now
+--2. Data Security - restricts access to a dataset by making a view available instead of a whole bunch of tables
+--3. Flexibility - Can be CRUDed the same way a regular table would be - CREATE, UPDATE, INSERT, DELETE, etc.
+--4. Simplified queries - hides the complexity of the underlying query
+--5. Updatability - with restrictions, you can update the underlying data, views CAN update regularly and you can also insert data into my view to (in certain cases) update my table.
+
+-- Updatable vs. Non-Updatable (read-only) Views
+--
+--if you make a view in your database and I don't have it in mine, then when I try to run my code, it won't run (matters for homework submissions)
+
+--if I change my table > my view changes
+--if I change data within my view > it will update my table 
+--EXCEPT when using aggregates - non updatable (can't update the underlying table)
+--WHY? if i say 'the average life expectency is 100 years old' you can't add a person to the aggregate
+
+--when would you want updateable views? When you have a form on your website that needs limited access to add people
+--Updateable (can be CRUDed) as long as:
+--1. Cannot include DISTINCT or TOP
+--2. Cannot include any AGGREGATE functions
+--3. Cannot include any calculated values
+--4. Cannot include a GROUP BY or HAVING clause
+--5. Cannot use UNION
+--updateable if it is simple --has columns, joins and maybe a WHERE clause.
+
+--How to modify a view? (check slide)
+
+
+--submit all view creation queries seperately from the creation and insertion scripts
