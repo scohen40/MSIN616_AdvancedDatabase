24774901db67638f06ec5b53e12c4f7b95c2f979
Sarah Cohen <cohen.sarah591@gmail.com>
2023-12-11 19:09:58 -0500

    project and notes files

---
+++ course_lecture_notes/Lesson_9.sql
@@ -0,0 +1,78 @@
+/*
+
+
+How to store a T-SQL Statement:
+1. Script
+2. Stored Procedure
+
+Example -
+*/
+--Creating
+USE AP;
+GO
+
+CREATE PROC spInvoiceReport
+AS 
+
+SELECT VendorName, InvoiceNumber, InvoiceDate, InvoiceTotal
+FROM Invoices i
+JOIN Vendors v ON i.VendorID = v.VendorID
+WHERE InvoiceTotal - CreditTotal - PaymentTotal > 0
+ORDER BY VendorName;
+
+--Running
+EXEC spInvoiceReport;
+
+--Stored Procedure that copies a table:
+USE AP;
+
+DROP PROC IF EXISTS spCopyInvoices;
+
+GO
+
+CREATE PROC spCopyInvoices
+AS 
+DROP TABLE IF EXISTS InvoiceCopy;
+	SELECT * INTO InvoiceCopy
+	FROM Invoices;
+
+EXEC spCopyInvoices
+
+/*
+
+Declaring Parameters - 
+@DateVar date  - input parameter that accepts a date value
+@VendorVar varchar(40) = NULL - optional input, parameter that accepts a character value
+@InvTotal money OUTPUT - output parameter that returns a monetary value
+
+3. User-defined function
+4. Trigger - stored procedure that runs automatically when various events happens.
+*/
+
+
+
+
+/*
+
+
+
+Transactions are the same thing as ?
+
+For parts 8 and 9 of the project you need to create a trigger and a stored  procedure - due before class next week.
+
+FINAL - on all the chapters:
+Part 1 = Queries
+for each question there will be two queries on anything and you can choose which one you want to write. pick 1.
+Part 2 = Short Answers
+general knowledge of sql, like normal forms, acid, etc.
+Part 3 = Database Design
+will be given a spreadsheet and asked to design a well designed, unmessed data, database
+
+Need to know the Normal Forms COLD. If you study 1 thing it should be that.
+
+
+
+
+
+
+*/
---
+++ course_work/FINAL_PROJECT/Library_Trigger_Procudure.sql
@@ -0,0 +1,39 @@
+/*
+Sarah Cohen and Abhilasha Hirawat
+This file contains the rough ideas and outlines for the Library's Trigger and Stored Procedure.
+*/
+
+/* TRIGGER: Update the return date of Book_Borrowed when the status is changed to 'RETURNED' */
+
+CREATE TRIGGER TRIG_BOOK_BORROWED_UPDATE
+ON BOOK_BORROWED
+AFTER UPDATE
+AS 
+BEGIN
+	--Check for books that are returned and the return date isn't yet set
+	IF EXISTS (SELECT * FROM inserted WHERE Returned_Date IS NULL AND Borrowed_Status = 'RETURNED')
+	BEGIN
+		UPDATE BOOK_BORROWED
+		SET Returned_Date = GETDATE()
+		FROM BOOK_BORROWED bb
+		INNER JOIN interted i ON bb.Borrowed_ID = i.Borrowed_ID
+		WHERE i.Returned_Date IS NULL AND i.Borrowed_Status = 'RETURNED';
+	END
+
+END
+GO
+
+
+/* STORED PROCEDURE: Check if a Library_Card is active before allowing a book to be borrowed on it. */
+
+CREATE PROCEDURE PROC_BOOK_BORROWED_INSERT
+	@CardID INT
+AS 
+BEGIN
+	--get the card details for the card from the Library_Card table
+	--check if the card is active
+	--if it's active, insert the data
+	--if it's not, raise an error and don't allow the entry into the Book_Borrowed table
+
+END
+GO
