195db08c95eea1b108d8f0f5efb90207791a080f
unknown <cohen.sarah591@gmail.com>
2023-12-18 14:43:36 -0500

    completed working stored procedure

--- course_work/FINAL_PROJECT/Library_Trigger_Procudure.sql
+++ course_work/FINAL_PROJECT/Library_Trigger_Procudure.sql
@@ -34,14 +34,23 @@ SELECT * FROM BOOK_BORROWED
 
 
 /* STORED PROCEDURE: Check if a Library_Card is active. */
-
-CREATE PROCEDURE PROC_BOOK_BORROWED_INSERT
+DROP PROC IF EXISTS SP_CHECK_CARD_ACTIVE;
+GO
+CREATE PROC SP_CHECK_CARD_ACTIVE
 	@CardID INT
 AS 
 BEGIN
-	--get the card details for the card from the Library_Card table
-	--check if the card is active
-	--print if is and expiration date
-
+	--get the card details for the card from the Library_Card table and check if the card is not expired
+	IF (SELECT Expiration_Date FROM LIBRARY_CARD WHERE Card_ID = @CardID) > GETDATE()	
+		--if is not expired print it and its expiration date
+		SELECT Card_ID, Expiration_Date
+		FROM LIBRARY_CARD
+		WHERE Card_ID = @CardID
+	ELSE 
+		PRINT 'CARD ' + CAST(@CardID AS VARCHAR) + ' IS EXPIRED'
 END
 GO
+--TEST SP_CHECK_CARD_ACTIVE
+EXEC SP_CHECK_CARD_ACTIVE @CardID = 100
+INSERT INTO LIBRARY_CARD (First_Name, Last_Name, [Address], City, [State], Zip, Phone, Birthdate, Card_Issue_Date, Is_Expired, Expiration_Date, Last_Renewal_Date, Balance_Due) VALUES ('Rhode', 'Dahl', '711 Dreary Lane', 'Placeberg', 'NA', '54321', '9876543210', '1900-02-12', '2021-01-10', 1, '2022-01-10', '2021-01-10', 0.00);
+EXEC SP_CHECK_CARD_ACTIVE @CardID = 111
